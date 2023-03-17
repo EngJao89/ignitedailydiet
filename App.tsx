4208f3308fcf287070982cadcdc2bd05b4ae710f
EngJao89 <eng_joao@outlook.com>
2023-03-16 23:16:09 -0300

    feat(dd07-static): implementa tela de estatística no app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'react-native'
-import { ThemeProvider } from 'styled-components'
-import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold, } from '@expo-google-fonts/nunito-sans'
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold, } from '@expo-google-fonts/nunito-sans';
 
-import theme from './src/theme'
-import { Loading } from '@components/Loading'
-import { Home } from '@screens/Home';
+import theme from './src/theme';
+import { Loading } from '@components/Loading';
+import { Statistic } from '@screens/Statistic';
 
 
 
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Home /> : <Loading/> }
+      {fontsLoaded ? <Statistic /> : <Loading/> }
     </ThemeProvider>
   );
 }
---
+++ src/screens/Statistic/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Page, Title } from "./styles";
+
+export function Statistic() {
+  return (
+    <Container>
+      <Page>
+        <Title>Estatísticas gerais</Title>
+      </Page>
+    </Container>
+  )
+}
